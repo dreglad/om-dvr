3ce310bc315c394b1358269b47cd694e62ab47c3
David Regla <dreglad@gmail.com>
2018-01-11 00:16:36 -0600

    upgraded webpack and vuetify + renamed project to "openmultimedia" + packages updated

--- src/main.js
+++ src/main.js
@@ -1,20 +1,14 @@
- // The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import Vuex from 'vuex'
 import router from './router'
 import Vuetify from 'vuetify'
 import App from './App'
 import store from './store'
-import VueWorker from 'vue-worker'
-
-// import('../node_modules/vuetify/dist/vuetify.min.css')
 
 Vue.config.productionTip = false
 
 Vue.use(Vuetify)
 Vue.use(Vuex)
-Vue.use(VueWorker)
 
 /* eslint-disable no-new */
 new Vue({
