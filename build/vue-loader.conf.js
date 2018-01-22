dec5793eb702598d108a752f4d8da86db24390fa
David Regla <dreglad@gmail.com>
2018-01-21 21:47:20 -0600

    updated packages with new PWA template

--- build/vue-loader.conf.js
+++ build/vue-loader.conf.js
@@ -1,18 +1,19 @@
 'use strict'
+
 const utils = require('./utils')
 const config = require('../config')
 const isProduction = process.env.NODE_ENV === 'production'
-const sourceMapEnabled = isProduction
-  ? config.build.productionSourceMap
-  : config.dev.cssSourceMap
 
 module.exports = {
-  loaders: utils.cssLoaders({
-    sourceMap: sourceMapEnabled,
-    extract: isProduction
-  }),
-  cssSourceMap: sourceMapEnabled,
-  cacheBusting: config.dev.cacheBusting,
+  loaders: {
+    ...utils.cssLoaders({
+      sourceMap: isProduction
+        ? config.build.productionSourceMap
+        : config.dev.cssSourceMap,
+      extract: isProduction
+    }),
+    i18n: '@kazupon/vue-i18n-loader'
+  },
   transformToRequire: {
     video: ['src', 'poster'],
     source: 'src',
